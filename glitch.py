c0dfc00a98b92a386c7259dae3f3d5cc23b8015c
Nicolas Noé <nicolas@niconoe.eu>
2015-03-12 19:00:11 +0100

    Working well with single image.

--- glitch.py
+++ glitch.py
@@ -1,20 +1,22 @@
 import os
 import uuid
 
-from glitches import glitch_replace
+from glitches import glitch_replace, glitch_randomize
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-SOURCE_IMAGE = os.path.join(CURRENT_DIR, "source.jpg")
+SOURCE_IMAGE = os.path.join(CURRENT_DIR, "code2.jpg")
 DESTINATION_DIR = os.path.join("/Users/nicolasnoe/glitch")
 
 
 def main():
     img_data = open(SOURCE_IMAGE, "rb").read()
+    source_file_extension = os.path.splitext(SOURCE_IMAGE)[1]
 
     while True:
         altered_image_data = glitch_replace(img_data)
-        dest_path = os.path.join(DESTINATION_DIR, (str(uuid.uuid4()) + '.jpg'))
+
+        dest_path = os.path.join(DESTINATION_DIR, (str(uuid.uuid4()) + source_file_extension))
 
         with open(dest_path, 'wb') as out:
             out.write(altered_image_data)
--- glitches.py
+++ glitches.py
@@ -1,4 +1,7 @@
+# Temps/lieux: action
+
 import random
+import uuid
 
 
 def get_random_start_and_end_points_in_file(file_data):
@@ -26,13 +29,52 @@ def splice_a_chunk_in_a_file(file_data):
     return file_data
 
 
+def _my_random_string(string_length):
+    """Returns a random string of length string_length."""
+    random = str(uuid.uuid4())
+    random = random.replace("-", "")
+    return random[0:string_length]
+
+
+def randomize_a_chunk_in_a_file(file_data):
+    start_point, end_point = get_random_start_and_end_points_in_file(file_data)
+
+    return file_data[:start_point] + _my_random_string(end_point - start_point) + file_data[end_point:]
+
+
 def glitch_replace(img_data):
     """ Splice a chunk, 1-5x.
 
-    Transformation taken from: https://github.com/artofwhatever/glitch-art-maker
+    Transformation taken from: https://github.com/artofwhatever/glitch-art-maker.
+
+    Results:
+
+    +: JPG, TGA, BMP (no unreadable images, often you can see the block moved, sometiomes other artifacts)
+    0: JPG2000, PDF
+    -: PNG
+
+    JPG remarks: 
+        - Gimp 2.8 exports are ugly (small "flys" or loss of resolution, nothing better) => Seems much better when we remove the "progressive" option
+        - LightRoom 5.6 exports: no color changes, just moved blocks
+        - Flickr exports: often good!
+        - It seems we have few changes in 4:4:4, medium changes in 4:2:2 (vertical or horizntal) and much changes in 4:2:0
+        (in that case, Preview is often unable to open the image, but Gimp is better)
     """
 
     for i in range(1, random.randint(2, 6)):
         img_data = splice_a_chunk_in_a_file(img_data)
+
+    return img_data
+
+
+def glitch_randomize(img_data):
+    """
+    Results:
+
+    -: JPG: Images are often truncated, sometimes unreadable, not really interesting artifacts.
+    0: TIF: Better results in Gimp than in Preview
+    """
+    for i in range(1, random.randint(2, 6)):
+        img_data = randomize_a_chunk_in_a_file(img_data)
 
     return img_data
